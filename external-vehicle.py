ac8fe5417e25e48ce8da10be6741c3a86829dfce
Esther.Cho <esther.mk.cho@gmail.com>
2022-01-06 16:34:36 +0900

    Calibrated external vehicle color threshold. Reduced loop time of the external vehicle.

--- external-vehicle.py
+++ external-vehicle.py
@@ -14,12 +14,13 @@ distance_sensor = UltrasonicSensor(Port.S4)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=118)
 
 # Initialize constants
-BLACK = 5
-WHITE = 75
+BLACK = 3
+WHITE = 63
 COLOR_THRESHOLD = (BLACK + WHITE) / 2
+LOOP_TIME = 100
 
 # Speed Configuration
-DRIVE_SPEED = 90
+DRIVE_SPEED = 200
 
 # Color PID Controller Constants
 COLOR_PROPORTIONAL_GAIN = 0.4
@@ -32,39 +33,26 @@ color_integral = 0.0
 color_derivative = 0.0
 color_last_deviation = 0.0
 
+watch = StopWatch()
+watch.reset()
+
 while True:
     time = watch.time()
     color = color_sensor.reflection()
-    distance = distance_sensor.distance()
 
     # Color PID Controller
-    # Calculate the deviation from the threshold.
     color_deviation = color - COLOR_THRESHOLD
-
-    # Calculate the integral.
-    # if deviation > -10 and deviation < 10:
-    #     integral = 0
-    # elif deviation * last_deviation < 0:
-    #     integral = 0
-    # else:
-    #     integral = integral + deviation
     color_integral = color_integral + color_deviation
-
-    # Calculate the derivative.
     color_derivative = color_deviation - color_last_deviation
-
-    # Calculate the turn rate.
     turn_rate = (COLOR_PROPORTIONAL_GAIN * color_deviation) + (COLOR_INTEGRAL_GAIN * color_integral) + (COLOR_DERIVATIVE_GAIN * color_derivative)
 
     robot.drive(DRIVE_SPEED, turn_rate)
     color_last_deviation = color_deviation
 
     end_time = watch.time()
-    
-    if LOG_DATA:
-        data.log(time, color, distance, turn_rate, DRIVE_SPEED)
 
-    wait_time = 150
-    if (end_time-time) < 150:
-        wait_time = 150-(end_time-time)
+    wait_time = 0
+    duration = end_time-time
+    if duration < LOOP_TIME:
+        wait_time = LOOP_TIME - duration
     wait(wait_time)
