dc5cd39c2dc327df84bd5f1cadcc1f38f8762885
Esther.Cho <esther.mk.cho@gmail.com>
2022-01-19 14:04:40 +0900

    Updated timeseries plotting with dynamic x-limit and font size of the labels.

--- analyses/csv2png.R
+++ analyses/csv2png.R
@@ -1,5 +1,8 @@
 rm(list = ls())
-setwd("~/Projects/selab/AV-FOT")
+
+if (substring(getwd(),nchar(getwd())-15,nchar(getwd()))=='/AV-FOT/analyses'){
+  setwd("../")
+}
 
 library(ggplot2)
 library(grid)
@@ -8,6 +11,9 @@ library(rpart)
 data_path <- 'data/'
 image_path <- 'analyses/images/'
 
+xlim_max <- 231
+font_size <- 20
+
 files <- list.files(data_path)
 
 for(file in files){
@@ -15,32 +21,36 @@ for(file in files){
   
   data$tick <- seq.int(nrow(data))
   color <- ggplot(data,aes(x=tick,y=color)) + geom_point(size=0.5,col=2) +
-    theme(axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),panel.background = element_blank(),axis.line=element_line(size=0.5)) +
-    xlim(c(0,300)) +
+    theme(axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),panel.background = element_blank(),axis.line=element_line(size=0.5), text=element_text(family="Times New Roman", face="bold", size=font_size)) +
+    xlim(c(0,xlim_max)) +
+    xlab('Time (x50ms)') +
+    ylab('Color') +
     ylim(c(0,100))
   
   distance <- ggplot(data,aes(x=tick,y=distance)) + geom_point(size=0.5,col=3) +
-    theme(axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),panel.background = element_blank(),axis.line=element_line(size=0.5)) +
-    xlim(c(0,300)) +
+    theme(axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),panel.background = element_blank(),axis.line=element_line(size=0.5), text=element_text(family="Times New Roman", face="bold", size=font_size)) +
+    xlim(c(0,xlim_max)) +
+    xlab('Time (x50ms)') +
+    ylab('Distance') +
     ylim(c(0,1000))
   
   angle <- ggplot(data,aes(x=tick,y=angle)) + geom_point(size=0.5,col=4) +
-    theme(axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),panel.background = element_blank(),axis.line=element_line(size=0.5)) +
-    xlim(c(0,300)) +
+    theme(axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),panel.background = element_blank(),axis.line=element_line(size=0.5), text=element_text(family="Times New Roman", face="bold", size=font_size)) +
+    xlim(c(0,xlim_max)) +
+    xlab('Time (x50ms)') +
+    ylab('Angle') +
     ylim(c(-25,25))
   
   speed <- ggplot(data,aes(x=tick,y=speed)) + geom_point(size=0.5,col=5) +
-    theme(panel.background = element_blank(),axis.line=element_line(size=0.5)) +
-    xlim(c(0,300)) +
-    ylim(c(0,500)) 
+    theme(panel.background = element_blank(),axis.line=element_line(size=0.5), text=element_text(family="Times New Roman", face="bold", size=font_size)) +
+    xlim(c(0,xlim_max)) +
+    xlab('Time (x50ms)') +
+    ylab('Speed') +
+    ylim(c(0,800)) 
   
   filename <- substring(file,1,nchar(file)-4)
-  png(file=paste(image_path, filename,".png",sep=""),width = 600, height = 350)
+  png(file=paste(image_path, filename,".png",sep=""),width = xlim_max*4, height = xlim_max*2)
   grid.newpage()
   grid.draw(rbind(ggplotGrob(color),ggplotGrob(distance),ggplotGrob(angle),ggplotGrob(speed)))
   dev.off()
 }
-
-
-
-
